afc4db28adb62a9211cebd1c9b7cc7e2f8849d67
Henry Ye Cao <henryyecao@henrys-mbp.home>
2024-07-28 11:48:54 -0400

    handlePost Unfinished

--- src/main/java/ca/yorku/eecs/AddActor.java
+++ src/main/java/ca/yorku/eecs/AddActor.java
@@ -2,6 +2,13 @@ package ca.yorku.eecs;
 
 import java.io.IOException;
 import org.json.*;
+import org.neo4j.driver.v1.AuthTokens;
+import org.neo4j.driver.v1.Driver;
+import org.neo4j.driver.v1.GraphDatabase;
+import org.neo4j.driver.v1.Record;
+import org.neo4j.driver.v1.Session;
+import org.neo4j.driver.v1.types.Node;
+import org.neo4j.driver.v1.Transaction;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
@@ -20,6 +27,7 @@ public class AddActor implements HttpHandler{
 	}
 	
 	public void handlePost(HttpExchange request) throws IOException, JSONException {
-		
+		String body = Utils.convert(request.getRequestBody()); // Convert request to String
+		JSONObject deserialized = new JSONObject(body); // Convert JSON to an object
 	}
 }
