54912ddcf070086d00734b1d2f3fdce9e573ff1f
Henry Ye Cao <henryyecao@henrys-mbp.home>
2024-07-29 01:07:27 -0400

    Fix handlePut method: Only create node when status code is 200

--- src/main/java/ca/yorku/eecs/AddActor.java
+++ src/main/java/ca/yorku/eecs/AddActor.java
@@ -9,6 +9,7 @@ import org.neo4j.driver.v1.Record;
 import org.neo4j.driver.v1.Session;
 import org.neo4j.driver.v1.types.Node;
 import org.neo4j.driver.v1.Transaction;
+import org.neo4j.driver.v1.StatementResult;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
@@ -20,6 +21,8 @@ public class AddActor implements HttpHandler{
 			// Only accept POST request
 			if (request.getRequestMethod().equals("PUT"))
 				this.handlePut(request);
+			else
+				request.sendResponseHeaders(404, -1); // If the request method is incorrect
 		}
 		catch (Exception e) {
 			e.printStackTrace();
@@ -34,28 +37,27 @@ public class AddActor implements HttpHandler{
 		String name = "";
 		String actorId = "";
 		
-		// Check if data has certain keys
-		if (data.has("name"))
+		if (data.has("name") && data.has("actorId")) {
 			name = data.getString("name");
+			actorId = data.getString("actorId");
+			statusCode = 200;
+		}
 		else
 			statusCode = 400; // If the client does not provide the required information
 		
-		if (data.has("actorId"))
-			actorId = data.getString("actorId");
-		else
-			statusCode = 400;
+		System.out.println("name: " + name);
+		System.out.println("actorId: " + actorId);
 		
-		System.out.println("name:" + name);
-		System.out.println("actorId" + actorId);
-		
-		// Run the PUT request with Neo4j
-		try (Session session = Utils.driver.session()) { // The parameter is to make sure the session is closed after it has finished
-			session.run(String.format("CREATE (Actor {name: %s, actor_id: %s});", name, actorId)); // Run the String/code input in Neo4j
-			System.out.println("Neo4j transaction successfully ran");
-		}
-		catch (Exception e) {
-			System.err.print("Caught Exception: " + e.getMessage());
-			statusCode = 500;
+		if (statusCode == 200) {
+			// Run the PUT request with Neo4j
+			try (Session session = Utils.driver.session()) { // The parameter is to make sure the session is closed after it has finished
+				session.run(String.format("CREATE (Actor {name: \"%s\", actor_id: \"%s\"})", name, actorId)); // Run the String/code input in Neo4j
+				System.out.println("Neo4j transaction successfully ran");
+			}
+			catch (Exception e) {
+				System.err.print("Caught Exception: " + e.getMessage());
+				statusCode = 500;
+			}
 		}
 		
 		// Send Response
--- src/main/java/ca/yorku/eecs/App.java
+++ src/main/java/ca/yorku/eecs/App.java
@@ -7,8 +7,7 @@ import com.sun.net.httpserver.HttpServer;
 public class App 
 {
     static int PORT = 8080;
-    public static void main(String[] args) throws IOException
-    {
+    public static void main(String[] args) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress("0.0.0.0", PORT), 0);
         
         server.createContext("/api/v1/addActor", new AddActor());
