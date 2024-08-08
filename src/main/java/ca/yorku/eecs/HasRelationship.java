c23af47658e5817a780e7371a3dfa7f4f0c55df9
Kamelia Dineva <kamelia.dineva@gmail.com>
2024-08-08 00:13:08 -0400

    temp changes

--- src/main/java/ca/yorku/eecs/HasRelationship.java
+++ src/main/java/ca/yorku/eecs/HasRelationship.java
@@ -1,6 +1,8 @@
 package ca.yorku.eecs;
 
 import java.io.IOException;
+import java.io.OutputStream;
+
 import org.json.*;
 import org.neo4j.driver.v1.*;
 import com.sun.net.httpserver.HttpExchange;
@@ -35,39 +37,47 @@ public class HasRelationship implements HttpHandler {
      * @throws JSONException
      */
     public void handleGet(HttpExchange request) throws IOException, JSONException {
-        String queryParams = request.getRequestURI().getQuery(); // Get query parameters from URL
+        String body = Utils.convert(request.getRequestBody());
+        JSONObject data = new JSONObject(body);
+        String response = null;
+
+       /* String queryParams = request.getRequestURI().getQuery(); // Get query parameters from URL
         JSONObject data = new JSONObject();
         for (String param : queryParams.split("&")) {
             String[] keyValue = param.split("=");
             data.put(keyValue[0], keyValue[1]);
-        }
+        } */
 
         int statusCode = this.validateRequestData(data);
 
         // Validate and process data, then check the relationship in the database
         if (statusCode == 200) {
-            String actorId = data.getString("actorId");
-            String movieId = data.getString("movieId");
 
-            System.out.println("Actor Id: " + actorId);
-            System.out.println("Movie Id: " + movieId);
+           // String actorId = data.getString("actorId");
+            //String movieId = data.getString("movieId");
+
+           // System.out.println("Actor Id: " + actorId);
+            //System.out.println("Movie Id: " + movieId);
 
             try {
-                boolean hasRelationship = this.checkRelationship(actorId, movieId);
+                response = this.hasRelationship(data.getString(Utils.movieIdProperty), data.getString(Utils.actorIdProperty));
+
+               /* boolean hasRelationship = this.checkRelationship(actorId, movieId);
                 JSONObject response = new JSONObject();
                 response.put("actorId", actorId);
                 response.put("movieId", movieId);
                 response.put("hasRelationship", hasRelationship);
-                this.sendResponse(request, 200, response.toString());
+                this.sendResponse(request, 200, response.toString()); */
             } catch (Exception e) { // Catch exception from checkRelationship
                 System.err.print("Caught Exception: " + e.getMessage());
                 statusCode = 500;
-                this.sendResponse(request, statusCode, "");
+               // this.sendResponse(request, statusCode, "");
             }
         } else {
             System.out.println("Bad request: The request format is incorrect or required parameters are missing");
-            this.sendResponse(request, statusCode, "");
+            //this.sendResponse(request, statusCode, "");
         }
+        this.sendResponse(request, statusCode, response);
     }
 
     /**
@@ -78,6 +88,23 @@ public class HasRelationship implements HttpHandler {
      */
     private int validateRequestData(JSONObject data) throws JSONException {
         try {
+            if (!data.has(Utils.movieIdProperty) || !data.has(Utils.actorIdProperty))
+                return 400; // Bad request
+
+            String movieId = data.getString(Utils.movieIdProperty);
+            String actorId = data.getString(Utils.actorIdProperty);
+
+            if (!this.findMovie(movieId) || !this.findActor(actorId))
+                return 404; // movie or info not found
+
+            return 200; // OK
+        }
+        catch (Exception e) {
+            System.err.print("Caught Exception: " + e.getMessage());
+            return 500; // Internal Server Error
+        }
+        /*
+        try {
             if (data.has("actorId") && data.has("movieId"))
                 return 200; // OK
             else
@@ -85,24 +112,60 @@ public class HasRelationship implements HttpHandler {
         } catch (Exception e) { // Catch exception from has
             System.err.print("Caught Exception: " + e.getMessage());
             return 500; // Internal Server Error
-        }
+        }*/
     }
 
+    private boolean findMovie(String movieId) throws Exception {
+        boolean exist = false;
+
+        try (Session session = Utils.driver.session()) {
+            try (Transaction tx = session.beginTransaction()) {
+                // Returns the movie that matches the movieId
+                String query = String.format("MATCH (m:%s) WHERE m.%s = $movieId RETURN m", Utils.movieLabel, Utils.movieIdProperty);
+                StatementResult results = tx.run(query, Values.parameters("movieId", movieId)); // Run query
+
+                // Check if results has any return
+                if (results.hasNext())
+                    exist = true;
+            }
+        }
+
+        return exist;
+    }
+
+    private boolean findActor(String actorId) throws Exception {
+        boolean exist = false;
+
+        try (Session session = Utils.driver.session()) {
+            try (Transaction tx = session.beginTransaction()) {
+                // Returns the movie that matches the movieId
+                String query = String.format("MATCH (a:%s) WHERE a.%s = $actorId RETURN m", Utils.actorLabel, Utils.actorIdProperty);
+                StatementResult results = tx.run(query, Values.parameters("actorId", actorId)); // Run query
+
+                // Check if results has any return
+                if (results.hasNext())
+                    exist = true;
+            }
+        }
+
+        return exist;
+    }
     /**
      * Check if the relationship exists between the actor and the movie in the database (neo4j)
      * @param actorId
      * @param movieId
      * @return boolean indicating if the relationship exists
-     */
+     *//*
     private boolean checkRelationship(String actorId, String movieId) throws Exception {
         boolean hasRelationship = false;
 
         try (Session session = Utils.driver.session()) {
             try (Transaction tx = session.beginTransaction()) {
                 // Query to check if the relationship exists
-                StatementResult result = tx.run("MATCH (a:Actor {actorId: $actorId})-[r:ACTED_IN]->(m:Movie {movieId: $movieId}) RETURN r",
-                        Values.parameters("actorId", actorId, "movieId", movieId));
-
+                String query = String.format("MATCH (a:%s {%s: $actorId})-[r:%s]-(m:%s {%s: $movieId}) RETURN h", Utils.actorLabel, Utils.actorIdProperty, Utils.hasRelationship, Utils.movieLabel, Utils.movieIdProperty);
+                StatementResult result = tx.run(query, Values.parameters("movieId", movieId, "actorId", actorId)); // Run query
+               // StatementResult result = tx.run("MATCH (a:Actor {actorId: $actorId})-[r:ACTED_IN]->(m:Movie {movieId: $movieId}) RETURN r",
+                 //       Values.parameters("actorId", actorId, "movieId", movieId));
                 if (result.hasNext()) {
                     hasRelationship = true;
                 }
@@ -110,7 +173,30 @@ public class HasRelationship implements HttpHandler {
         }
 
         return hasRelationship;
+    } */
+    private String hasRelationship(String movieId, String actorId) throws Exception {
+        String response = null;
+
+        try (Session session = Utils.driver.session()) {
+            try (Transaction tx = session.beginTransaction()) {
+                String query = String.format("MATCH (a:%s {%s: $actorId}), (m:%s {%s: movieId}) OPTIONAL MATCH (a)-[r:%s]-(m) RETURN m.%s AS movieId, a.%s AS actorId, EXISTS ((a)-[:%s]-(m)) AS hasRelationship", Utils.actorLabel, Utils.actorIdProperty, Utils.movieLabel, Utils.movieIdProperty, Utils.actedInRelationship, Utils.movieIdProperty, Utils.actorIdProperty, Utils.actedInRelationship);
+                StatementResult results = tx.run(query, Values.parameters("movieId", movieId, "actorId", actorId));  // Use "AS" to rename key, since it will appear the name in the JSON
+
+                JSONObject json = new JSONObject();
+
+                Record record = results.next();
+
+                json.put("movieId", record.get("movieId").asString());
+                json.put("actorId", record.get("actorId").asString());
+                json.put("hasRelationship", record.get("hasRelationship").asBoolean());
+
+                response = json.toString();
+            }
+        }
+
+        return response;
     }
+
 
     /**
      * Send status code back to the client
@@ -120,9 +206,20 @@ public class HasRelationship implements HttpHandler {
      * @throws IOException
      */
     private void sendResponse(HttpExchange request, int statusCode, String response) throws IOException {
-        byte[] bytes = response.getBytes();
+       /* byte[] bytes = response.getBytes();
         request.sendResponseHeaders(statusCode, bytes.length); 
         request.getResponseBody().write(bytes);
-        request.getResponseBody().close();
+        request.getResponseBody().close();*/
+        if (statusCode == 200) {
+            request.sendResponseHeaders(statusCode, response.length()); // .sendResponseHeaders(Status code, Response length)
+
+            // Overwrite the response body with the response
+            OutputStream os = request.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+        else
+            request.sendResponseHeaders(statusCode, -1);
     }
+
 }
