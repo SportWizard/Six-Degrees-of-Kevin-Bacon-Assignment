35abf68f2c70e45200f91b3aa982f685e8ca3deb
Henry Ye Cao <henryyecao@henrys-mbp.home>
2024-08-08 02:48:12 -0400

    Change to BFS class

--- src/main/java/ca/yorku/eecs/App.java
+++ src/main/java/ca/yorku/eecs/App.java
@@ -19,7 +19,7 @@ public class App {
         server.createContext("/api/v1/getActor", new GetActor());
         server.createContext("/api/v1/hasMovieInfo", new HasMovieInfo());
         server.createContext("/api/v1/computeBaconNumber", new ComputeBaconNumber());
-//        server.createContext("/api/v1/computeBaconPath", new ComputeBaconPath());
+        server.createContext("/api/v1/computeBaconPath", new ComputeBaconPath());
         server.createContext("/api/v1/getMovie", new GetMovie());
         server.createContext("/api/v1/getYear", new GetYear());
         server.createContext("/api/v1/hasRelationship", new HasRelationship());
